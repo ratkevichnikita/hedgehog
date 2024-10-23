17bd45f7bd008900ffdba733835f6e3f92994c5f
nikita <nikita.ratkevich91@mail.ru>
2024-10-23 23:30:25 +0200

    added fixed menu

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.header__nav ul li a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+    targetSection.scrollIntoView({
+      behavior: 'smooth', 
+      block: 'start' 
+    });
+  });
+});
+
+
+window.addEventListener('scroll', () => {
+  let scrollPosition = window.scrollY;
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+
+    if (scrollPosition >= (sectionTop - 100) && scrollPosition < sectionTop + sectionHeight) {
+      const currentId = section.getAttribute('id');
+      navLinks.forEach((link) => {
+        link.closest('LI').classList.remove('active');
+      });
+      document.querySelector(`.header__nav ul li a[href="#${currentId}"]`).closest('LI').classList.add('active');
+    }
+  });
+});
+
+
+
+const headerInner = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 50) {
+    headerInner.classList.add('sticky');
+  } 
+  if(window.scrollY === 0) {
+    headerInner.classList.remove('sticky');
+  }
+});
